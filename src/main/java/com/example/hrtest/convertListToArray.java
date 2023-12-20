56d8955035c8729f6152a04a309cd0c9eabf0f58
LathaGurusamy <lathalearningwithguvi@gmail.com>
2023-12-20 23:45:41 +0530

    f

---
+++ src/main/java/com/example/hrtest/convertListToArray.java
@@ -0,0 +1,30 @@
+package com.example.hrtest;
+import java.util.ArrayList;
+public class convertListToArray {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> sampleList = new ArrayList<String>();
+
+        sampleList.add("California");
+        sampleList.add("Texas");
+        sampleList.add("Illinois");
+        sampleList.add("Massachusetts");
+        sampleList.add("Florida");
+        sampleList.add("Virginia");
+        sampleList.add("Colorado");
+
+
+        System.out.println("Elements of List: " + sampleList);
+
+        String[]arr = new String [sampleList.size()];
+
+        sampleList.toArray(arr);
+
+        System.out.print("Elements of Array: ");
+        for (int i = 0 ; i < arr.length ; i++){
+            System.out.print(arr[i] + "  ");
+        }
+    }
+}
